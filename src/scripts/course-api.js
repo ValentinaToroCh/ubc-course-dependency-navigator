f6aef8334fc5fdc6df4476c9692f604553f48111
UBC Student <valetch@students.cs.ubc.ca>
2024-07-18 15:34:38 -0700

    single course search functionality
    only new course searchers are displayed. A button for pre-requisites has been added when a course has been searched. When a new course is searched that button is updated based on the course ID

--- src/scripts/course-api.js
+++ src/scripts/course-api.js
@@ -1,4 +1,12 @@
-// eventually migrate course api functions here
+// global variables
+let prevSearchedCourseCode;
+let searchedCourse;
+let searchedCourseCode;
+let searchedCoursePre;
+let searchedCourseDep;
+
+// gets all courses, parsed onto JSON and displays the course code and 
+// corresponding credits of all courses offerred in UBC
 function getAllCourses(){
     fetch('https://ubcexplorer.io/getAllCourses')
     .then(res => {
@@ -8,32 +16,62 @@ function getAllCourses(){
         console.log(data);
         data.forEach(course => {
             document.getElementById("courseList").innerHTML += "<li>" + course.code + "</li>" + "<ul>" + "<li>credits:" + course.cred + "</li>" + "</ul>";
-            console.log("done");
+            console.log("get all courses: done");
         });
     })
 }
 
-// get array containing: course Subject and Code 
+// get array containing: course Subject and Code to be used on the API
+// searching for specific courses
 function getCourseSubjectAndCode(){
     const course = document.getElementById('courseInput');
     const courseArray = course.value.toUpperCase().split(" ");
     return courseArray;
 }
 
+// dynamically adds a button with a text and links it to a corresponding
+// function
+function addButton(buttonText, buttonFn){
+    if (prevSearchedCourseCode != null){
+        const child = document.getElementById(prevSearchedCourseCode);
+        document.body.removeChild(child);
+    }
+    // console.log("button id to delete: " + prevSearchedCourseCode);
+    const btn = document.createElement('button');
+    btn.innerHTML = buttonText;
+    btn.id = searchedCourseCode;
+    document.body.appendChild(btn);
+    btn.addEventListener('click', () => {
+        buttonFn();
+      });
+    // console.log("created button id: " + btn.id);
+}
+
+function onPreReqsButtonClick(){
+     alert('Button pressed!' + searchedCourseCode);
+}
+    
+/*  gets course from suer input and searches 
+    TODO: expand the function so that it can work with a course as a parameter instead */
+
 function searchCourse(){
     courseArray = getCourseSubjectAndCode();
-    fetch('https://ubcexplorer.io/getCourseInfo/' + courseArray[0] + '%20' + courseArray[1])
+    searchedCourse = fetch('https://ubcexplorer.io/getCourseInfo/' + courseArray[0] + '%20' + courseArray[1]);
+    searchedCourse
     .then(res => {
         return res.json();
     })
     .then(cData => {
         console.log(cData);
-        document.getElementById("courseList").innerHTML +=
+        searchedCourseCode = cData.code;
+        document.getElementById("courseList").innerHTML =
             "<li>" + cData.code + "</li>" +
             "<ul>" + "<li>Course name: " + cData.name + "</li>" + "</ul>" +
             "<ul>" + "<li>Credits: " + cData.cred + "</li>" + "</ul>" +
             "<ul>" + "<li>Description: " + cData.desc + "</li>" + "</ul>" +
             "<ul>" + "<li>Pre-requisites: " + cData.prer + "</li>" + "</ul>";
-            // "<ul>" + "<li>Depedents: " + cData.dep + "</li>" + "</ul>";
+        addButton("Get pre-requisites for: " + searchedCourseCode, onPreReqsButtonClick);
+        prevSearchedCourseCode = searchedCourseCode;
     })
+    
 }
