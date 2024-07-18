5104d642cfc33647df724f3b70681eb5c174dc1b
UBC Student <valetch@students.cs.ubc.ca>
2024-07-17 19:00:59 -0700

    migrate api code to separate js file
    migrated all functions related to the api to a separate JS file

--- src/scripts/course-api.js
+++ src/scripts/course-api.js
@@ -1,1 +1,39 @@
 // eventually migrate course api functions here
+function getAllCourses(){
+    fetch('https://ubcexplorer.io/getAllCourses')
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+        data.forEach(course => {
+            document.getElementById("courseList").innerHTML += "<li>" + course.code + "</li>" + "<ul>" + "<li>credits:" + course.cred + "</li>" + "</ul>";
+            console.log("done");
+        });
+    })
+}
+
+// get array containing: course Subject and Code 
+function getCourseSubjectAndCode(){
+    const course = document.getElementById('courseInput');
+    const courseArray = course.value.toUpperCase().split(" ");
+    return courseArray;
+}
+
+function searchCourse(){
+    courseArray = getCourseSubjectAndCode();
+    fetch('https://ubcexplorer.io/getCourseInfo/' + courseArray[0] + '%20' + courseArray[1])
+    .then(res => {
+        return res.json();
+    })
+    .then(cData => {
+        console.log(cData);
+        document.getElementById("courseList").innerHTML +=
+            "<li>" + cData.code + "</li>" +
+            "<ul>" + "<li>Course name: " + cData.name + "</li>" + "</ul>" +
+            "<ul>" + "<li>Credits: " + cData.cred + "</li>" + "</ul>" +
+            "<ul>" + "<li>Description: " + cData.desc + "</li>" + "</ul>" +
+            "<ul>" + "<li>Pre-requisites: " + cData.prer + "</li>" + "</ul>";
+            // "<ul>" + "<li>Depedents: " + cData.dep + "</li>" + "</ul>";
+    })
+}
